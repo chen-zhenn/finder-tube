1c634133de13e694213a5dd1c7e4ed583a8d4f6f
carlos <carlos@visie.com.br>
2021-10-19 19:50:27 -0300

    Back-end -> Add route to search videos...

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Initial route!!!')
+app.use(cors())
+
+app.get('/videos', async (req, res) => {
+
+  const baseURL = 'https://youtube.googleapis.com/youtube/v3'
+  const resource= 'search'  
+  const { key, part, type, q } = req.query
+  const endpoint = `${baseURL}/${resource}?part=${part}&q=${q}&key=${key}`
+  const request = JSON.stringify({'Authorization': `${key}`})
+
+  await axios.get(endpoint, request)
+    .then(response => res.json(response.data))
+    .catch(error => console.log(error))
+  
 })
 
 app.listen(port, () => {
